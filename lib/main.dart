f0f989e24b90f9a29e9c17981558a7ba7984cde8
AndresFelipe077 <pizoluligoa@gmail.com>
2023-09-03 16:20:04 -0500

    Add message in screen with colors theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/config/theme/app_theme.dart';
-import 'package:flutter_chat/presentation/chat/chat_screen.dart';
+import 'package:flutter_chat/presentation/screens/chat/chat_screen.dart';
 
 void main() => runApp(const MyApp());
 
